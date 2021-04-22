829cea04e410686e8248b97bb031424bc265ebbe
Bryan Arias <BAriasA@iingen.unam.mx>
2021-04-22 18:26:55 +0000

    [HERENCIA]

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -15,7 +15,7 @@
     'category': 'Training',
     'version': '0.1',
     
-    'depends': ['base'],
+    'depends': ['sale'],
     
     'data':[
         'security/academy_security.xml',
@@ -23,6 +23,7 @@
         'views/course.xml',
         'views/academy_menuitems.xml',
         'views/session_views.xml',
+        'views/sale_views_inherit.xml'
         
     ],
     
